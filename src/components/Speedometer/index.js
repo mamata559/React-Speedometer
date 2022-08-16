2bf014e06963077d630501283f3c2dad5e806955
mamatha <mamata.barupati@gmail.com>
2022-08-16 05:32:48 +0000

    Commited State object on Speedometer

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -3,7 +3,27 @@ import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
+  state = {speed: 0}
+
+  onClickAccelerate = () => {
+    const {speed} = this.state
+
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
+  }
+
+  onClickBrake = () => {
+    const {speed} = this.state
+
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
   render() {
+    const {speed} = this.state
+
     return (
       <div className="bg-container">
         <h1 className="heading">SPEEDOMETER</h1>
@@ -12,17 +32,21 @@ class Speedometer extends Component {
           alt="SpeedoMeter"
           className="image1"
         />
-        <h2 className="heading heading1">Speed is 0mph</h2>
+        <h2 className="heading heading1">Speed is {speed}mph</h2>
         <p className="para">Min Limit is 0mph, Max Limit is 200mph</p>
         <div className="button-container">
           <button
             type="button"
             className="button-element button1"
-            onClick="onClickAccelerate"
+            onClick={this.onClickAccelerate}
           >
             Accelerate
           </button>
-          <button type="button" className="button-element">
+          <button
+            type="button"
+            className="button-element"
+            onClick={this.onClickBrake}
+          >
             Apply Brake
           </button>
         </div>
